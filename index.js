015d193b9edd141e87bf6dfffa07b115685b95b3
Khey73 <msilvarubios2@hotmail.com>
2023-01-04 18:38:15 -0600

    Se agregaron parte de los estilos de la ventana pop y la funcionalidad en el boton

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let listaProyectos = [];
 
 // agregando informacion al array
 
-listaProyectos.push({id: 1, proyecto: "Pagina responsive + Manejo del DOM", link: "./prueba.html"})
+listaProyectos.push({id: 1, proyecto: "Pagina responsive + Manejo del DOM", link: "./proyectos/pagina-responsive-Dom"})
 listaProyectos.push({id: 2, proyecto: "Contador de Macronutrientes", link: "./prueba2.html"})
 
 
@@ -21,3 +21,18 @@ for(dato of listaProyectos){
     divLista.append(parrafos);
 }
 
+
+//boton
+
+let btn = document.querySelector("#Contactame");
+let fondoVentana = document.querySelector(".fondoVentana");
+let cerrar = document.querySelector("#cerrar");
+
+
+btn.addEventListener("click", ventanaPop);
+
+function ventanaPop(){
+    fondoVentana.classList.toggle("hidden");
+}
+
+cerrar.addEventListener("click", ventanaPop)
